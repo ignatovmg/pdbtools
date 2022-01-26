4954969b346fd33e45f01bb1b4ebe867fb442a94
ignatovmg <mikhail.ignatov@stonybrook.edu>
2022-01-26 15:13:12 -0500

    Fixed a bug in all-sdf.index generation, it omitted ions

--- data/download_all.sh
+++ data/download_all.sh
@@ -5,7 +5,7 @@ oldpwd=$(pwd)
 mkdir -p data/ligand_expo && cd data/ligand_expo
 wget http://ligand-expo.rcsb.org/dictionaries/{Components-pub.sdf.gz,all-sdf.sdf.gz}
 gzip -d all-sdf.sdf.gz Components-pub.sdf.gz
-python $oldpwd/../pdbtools/database.py all-sdf.sdf --sep_start '(^.{4}_.{3}_[0-9]+_.+_[0-9]+_.+)\n' -w all-sdf.index
+python $oldpwd/../pdbtools/database.py all-sdf.sdf --sep_start '(^.{4}_[A-Z0-9]{1,3}_[0-9]+_.+_[0-9]+_.+)\n' -w all-sdf.index
 python $oldpwd/update_ligand_expo.py
 python $oldpwd/update_amino_acids.py
 cd $oldpwd
